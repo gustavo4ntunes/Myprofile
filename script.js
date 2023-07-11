322973b550a9a95faa2f2f6560f42f26c0c1d3b7
Gustavo Antunes <gustavoantunes.gr8@gmail.com>
2023-07-11 12:04:38 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    // ou
+   // if(html.classList.contains('light')) {
+   // html.classList.remove('light')
+   // } 
+   // else {
+   // html.classList.add('light')
+   // } 
+
+    //troca da imagem
+   // pegar a tag img
+   const img = document.querySelector("#profile img")
+
+   //substituir a imagem
+    if(html.classList.contains("light")) {
+        //se tiver light mode, adicionar aimagem light
+        img.setAttribute("src", "./assets/assets/avatar-light.png")
+        img.setAttribute("alt", "foto Mayk Brito, com oculos escuros, roupas pretas e fundo colorido roxo e azul")
+    }
+    else {
+        img.setAttribute("src", "./assets/assets/avatar.png")
+        img.setAttribute("alt", "foto Mayk Brito, com oculos de grau, roupas pretas e fundo colorido roxo e azul")
+    } 
+   //se tiver sem light mode, manter a imagem normal
+
+
+}
